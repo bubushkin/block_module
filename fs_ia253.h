be87e757e6ee126ec8c12c4c4aaa34890f2dde4c
bubushkin <bubushkin@users.noreply.github.com>
2018-05-07 20:55:47 -0400

    re-organizing.
    Added inode structure; added endianness check

--- fs_ia253.h
+++ fs_ia253.h
@@ -3,6 +3,7 @@
 
 #include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/types.h>
 
 const unsigned long FSIA253_MAGIC_NUM = 0xBBBBCCCC9999
 
@@ -11,6 +12,33 @@ struct file_operations fs_ia253_file_ops;
 
 struct kmem_cache *inode_kcache;
 
+
+struct meta_disk_inode {
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		__le32	e_i_first;
+		__le32	e_i_blocks;
+		__le32	e_i_size;
+		__le32	e_i_gid;
+		__le32	e_i_uid;
+		__le32	e_i_mode;
+		__le64	e_i_ctime;
+	#else
+		__be32	e_i_first;
+		__be32	e_i_blocks;
+		__be32	e_i_size;
+		__be32	e_i_gid;
+		__be32	e_i_uid;
+		__be32	e_i_mode;
+		__be64	e_i_ctime;
+	#endif
+};
+
+
+/*
+	filesystem
+*/
+void fs_inode_init(void *);
+struct dentry *fs_mount(struct file_system_type *, int, char const *, void *);
 
 struct fs_param{
 
@@ -22,42 +50,47 @@ struct fs_param{
 
 } p_fs_params;
 
-
 struct fs_inode {
 	struct inode fs_inode;
 	unsigned long fsblock;
 };
 
-struct fs_ia253_sb {
+/*
+	superblock
+*/
+
+struct super_operations ia253_super_ops;
+
+int init_superblock(struct super_block *, void *, int)
+struct fs_ia253_sb *init_logical_block(struct super_block *);
+
+struct meta_disk_sb {
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		__le32	e_magic_num;
+		__le32	e_block_size;
+		__le32	e_root_inode;
+		__le32	e_inode_blocks;
+	#else
+		__be32	e_magic_num;
+		__be32	e_block_size;
+		__be32	e_root_inode;
+		__be32	e_inode_blocks;
+	#endif
+};
+
+struct fs_ia253_sb { //logical block structure!
 
 	unsigned long inode_blocks;
 	unsigned long block_size;
 	unsigned long magic_num;
 	unsigned long root;
-	unsigned long inodes_per_blk;
-
+	unsigned long inode_blocks;
 };
-
-/*
-	filesystem
-*/
-void fs_inode_init(void *);
-struct dentry *fs_mount(struct file_system_type *, int, char const *, void *);
-
-
-/*
-	superblock
-*/
-
-int init_superblock(struct super_block *, void *, int)
-
-
 
 /*
 	cache initializers/destructors
 */
 int inode_cache_alloc(void);
 void fs_release_all(void);
-
 
 #endif
